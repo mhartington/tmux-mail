6bcfbaee820fa453fae59a16ff5a89fac63c6d6b
mhartington <mikehartington@gmail.com>
2015-12-25 13:17:02 -0500

    update

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -2,7 +2,6 @@
 
 function getGmail(){
   mailrc=~/.config/mailrc.txt
-  echo -e "user=mikehartington@gmail.com\npassword=fhvfxioqhpuottve" > ${mailrc}
 # while true ; do
   wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --config "$mailrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
 #   sleep 300
