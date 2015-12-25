5c1c358cc370d3ce58b83c01b316ef05ae35ccab
mhartington <mikehartington@gmail.com>
2015-12-25 14:27:37 -0500

    updates

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -3,13 +3,12 @@
 getGmail(){
   mailrc=~/.config/mailrc.txt
 # while true ; do
-  # wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --config "$mailrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
+  wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --config "$mailrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
 #   sleep 300
 # done
 }
 
 main() {
-  # getGmail
-  echo "Hello"
+  getGmail
 }
 main
