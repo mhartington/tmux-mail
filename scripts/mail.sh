464355ea6cbc039336bcd98e57f3d66eadfaddcd
Mike Hartington <mikehartington@gmail.com>
2016-05-05 08:10:43 -0400

    feat(): only render when there is email

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -2,13 +2,12 @@
 
 getGmail(){
   mailrc=~/.config/mailrc.txt
-# while true ; do
-  wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --config "$mailrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
-#   sleep 300
-# done
+  OUTPUT="$(wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --config "$mailrc" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')"
+  if [ "$OUTPUT" == "0" ]
+  then
+    return 0
+  else
+    echo "✉ ${OUTPUT}"
+  fi
 }
 
-main() {
-  getGmail
-}
-main
